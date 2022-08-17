96a16a5ff3fb215fac35159e997d1fb90ca456c9
dwaynepayton12 <68318057+dwaynepayton12@users.noreply.github.com>
2022-08-17 11:34:11 -0500

    Create movement_factory.rb

---
+++ movement/movement_factory.rb
@@ -0,0 +1,9 @@
+class MovementFactory
+    def initialize(speciality)
+      @movement_class = self.class.const_get("#{speciality}Movement")
+    end
+  
+    def build
+      @movement_class.new
+    end
+end
