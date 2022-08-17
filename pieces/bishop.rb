ff2345e11caa1b047ee7c89a0e4e162c5fff6680
dwaynepayton12 <68318057+dwaynepayton12@users.noreply.github.com>
2022-08-17 11:39:06 -0500

    Create bishop.rb

---
+++ pieces/bishop.rb
@@ -0,0 +1,15 @@
+require_relative 'piece'
+
+# logic for bishop chess piece
+class Bishop < Piece
+  def initialize(board, args)
+    super(board, args)
+    @symbol = " \u265D "
+  end
+
+  private
+
+  def move_set
+    [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+  end
+end
