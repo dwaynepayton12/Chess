bd3ea54d05c12ec20ca191012157cd394d747dc7
dwaynepayton12 <68318057+dwaynepayton12@users.noreply.github.com>
2022-08-17 11:36:40 -0500

    Create rook.rb

---
+++ pieces/rook.rb
@@ -0,0 +1,13 @@
+# logic for rook chess piece
+class Rook < Piece
+  def initialize(board, args)
+    super(board, args)
+    @symbol = " \u265C "
+  end
+
+  private
+
+  def move_set
+    [[0, 1], [0, -1], [-1, 0], [1, 0]]
+  end
+end
